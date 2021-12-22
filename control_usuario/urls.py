1746ed1aed38efefb18abb11d2c9b496f03e5507
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2021-12-21 23:54:19 -0600

    templates bienvenida, login y home conectadas

--- control_usuario/urls.py
+++ control_usuario/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.Bienvenida_view, name="Bienvenida_view"),
+    path('login/', views.login_view, name="login_view"),
+    path('home/', views.Home_view, name="Home_view"),
 ]
--- control_usuario/views.py
+++ control_usuario/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def Bienvenida_view(request):
-    return render(request, 'Bienvenida.html')
+    return render(request, 'Bienvenida.html', {'var': 'bienvenida'})
+
+def Home_view(request):
+    return render(request, 'Home.html')
+
+def login_view(request):
+    return render(request, 'login.html', {'var': 'login'})
