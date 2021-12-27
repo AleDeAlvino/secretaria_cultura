cc5305c6b1826c04cefd4710aa12fe9348fb92ec
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2021-12-27 10:00:50 -0600

    funcion agregar pais con continente lista

--- control_catalogos/models.py
+++ control_catalogos/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Paises(models.Model):
     pais = models.CharField(max_length = 50, unique=True)
+    continente = models.CharField(max_length = 10)
     def __str__(self):
          return "{}".format(self.pais)
--- control_catalogos/views.py
+++ control_catalogos/views.py
@@ -14,15 +14,22 @@ def cat_paises_view(request):
     if request.method == 'POST':
         print(request.POST['pais'])
         if request.POST['pais'] != '' :
-            try:
-                pais, created = Paises.objects.get_or_create(pais=request.POST['pais'])
-                if created:
-                    pais.save()
-                    messages.success(request, 'Se agregó el país correctamente')
+            conti = request.POST['select_Continente']
+            if conti != 'Selecciona un continente':
+                if conti == 'Asia' or conti == 'América' or conti == 'África' or conti == 'Antartida' or conti == 'Europa' or conti == 'Oceanía':
+                    try:
+                        pais, created = Paises.objects.get_or_create(pais=request.POST['pais'], continente=request.POST['select_Continente'])
+                        if created:
+                            pais.save()
+                            messages.success(request, 'Se agregó el país correctamente')
+                        else:
+                            messages.error(request, 'Ya existe el país ' + request.POST['pais'])
+                    except IntegrityError:
+                        messages.error(request, 'Ya existe el país ')
                 else:
-                    messages.error(request, 'Ya existe el país ' + request.POST['pais'])
-            except IntegrityError:
-                messages.error(request, 'Ya existe el país ')
+                    messages.error(request, 'No existe ese continente')
+            else:
+                messages.error(request, 'No has seleccionado ningun continente')
         else:
             messages.error(request, 'El campo país está vacío')
     return render(request, 'catalogo_paises.html')
